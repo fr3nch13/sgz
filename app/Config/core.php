f74ef1c095c7fe6e51a2488d45b84ef8f6563d7e
fr3nch13 <Brian French>
2014-05-19 15:39:04 -0700

    Initial Setup

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'Hufg97gjUFGEbg45607JGEughr75');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '93845702353209687230986734095674590');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
